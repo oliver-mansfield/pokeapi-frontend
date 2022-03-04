f898194f5ae100b3baba08c8037bb880b988060b
oliver-mansfield <olivermansfield.dev@gmail.com>
2022-03-04 14:16:31 +0000

    using dummy data for dev

--- utility/getAllNames.js
+++ utility/getAllNames.js
@@ -1,32 +1,15 @@
+import pokemonData from "./pokemonData";
+
 const getAllNames = async () => {
-	//getAllPokemon json
-	//query each additional endpoint eg https://pokeapi.co/api/v2/pokemon/1/
-	//store 'species.name' for name eg 'bulbasaur'
-	// store 'sprites.other.dream_world.front_default' for SVG url
-	//return array of pokemon [{name: 'bulbasaur', thumbnailImage: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"}]
+	//return dummy data during dev to avoid getting IP banned for spamming api
+	return pokemonData.results;
 
-	//query https://pokeapi.co/api/v2/pokemon?limit=151
-
-	//"results": [
-	// { "name": "bulbasaur", "url": "https://pokeapi.co/api/v2/pokemon/1/" },
-	// { "name": "ivysaur", "url": "https://pokeapi.co/api/v2/pokemon/2/" },
-	// { "name": "venusaur", "url": "https://pokeapi.co/api/v2/pokemon/3/" },
-	//]
-
-	//loop results array
-	//query
-
-	//or https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png
-	//https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/crystal/1.png
-
-	// return [];
-
-	return await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-		.then((response) => response.json())
-		.then((data) => {
-			console.log(data.results);
-			return data.results;
-		});
+	// return await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+	// 	.then((response) => response.json())
+	// 	.then((data) => {
+	// 		console.log(data.results);
+	// 		return data.results;
+	// 	});
 };
 
 export default getAllNames;
