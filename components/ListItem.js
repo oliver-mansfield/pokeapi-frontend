1f78635a1da531b61c467e65e0f3295644b68428
oliver-mansfield <olivermansfield.dev@gmail.com>
2022-03-04 13:57:35 +0000

    destructuring props

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,10 +1,10 @@
-const ListItem = (props) => {
+const ListItem = ({ name, index } = props) => {
 	return (
 		<>
-			<p>{props.name}</p>
+			<p>{name}</p>
 			<img
 				src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
-					props.index + 1
+					index + 1
 				}.svg`}
 			/>
 		</>
