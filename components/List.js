e59d5a2a77a8aa33c673d216a3c0040b7201cb00
oliver-mansfield <olivermansfield.dev@gmail.com>
2022-03-06 12:27:25 +0000

    adding animation lib, adding overlay

--- components/List.js
+++ components/List.js
@@ -1,9 +1,10 @@
 import getAllNames from "../utility/getAllNames";
 import { useState, useEffect } from "react";
-import ListItem from "../components/ListItem";
+import ListItem from "./ListItem";
 
 const List = () => {
 	const [names, setNames] = useState([]);
+	const [showOverlay, setShowOverlay] = useState(false);
 
 	useEffect(() => {
 		console.log("run it");
@@ -20,8 +21,36 @@ const List = () => {
 
 	return (
 		<>
-			<h2>List</h2>
-			{listItems}
+			<div className="list-outer">
+				<div className="list">{listItems}</div>
+
+				{showOverlay ? <div className="overlay"></div> : null}
+			</div>
+
+			<style jsx>{`
+				.list {
+					display: grid;
+					grid-template-columns: 1fr 1fr 1fr;
+					row-gap: 20px;
+					column-gap: 20px;
+					position: relative;
+				}
+
+				.list-outer {
+					background: grey;
+				}
+
+				.overlay {
+					background-color: red;
+					opacity: 0.5;
+					position: fixed;
+					top: 0;
+					left: 0;
+					width: 100%;
+					height: 100%;
+					z-index: 9;
+				}
+			`}</style>
 		</>
 	);
 };
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,28 +1,44 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useRef } from "react";
+import { motion } from "framer-motion";
 
 const ListItem = ({ name, index } = props) => {
+	const itemRef = useRef(name);
 	const router = useRouter();
 
 	const handleClick = (e) => {
-		console.log("clicked " + name);
+		// console.log("clicked ref" + itemRef.current);
+		console.log("clicked index" + index);
 		e.preventDefault();
-		router.push(`/${name}`);
+
+		// router.push(`/${name}`);
 	};
 
 	return (
-		<button onClick={handleClick}>
-			<div>
-				<p>{name}</p>
-				<Image
-					src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
-						index + 1
-					}.svg`}
-					width="200"
-					height="200"
-				/>
-			</div>
-		</button>
+		<div className="list-item" onClick={handleClick}>
+			<img
+				src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+					index + 1
+				}.svg`}
+				width="200"
+				height="200"
+				className="image"
+			/>
+
+			<style jsx>{`
+				.list-item {
+					border: 1px solid black;
+					width: 100%;
+					background-color: white;
+				}
+
+				.image {
+					display: block;
+					margin: 0 auto;
+				}
+			`}</style>
+		</div>
 	);
 };
 
