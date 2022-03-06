4787bb83d0e5b98740d08ffc4f0dc6f49f5bd05f
oliver-mansfield <olivermansfield.dev@gmail.com>
2022-03-06 15:51:05 +0000

    half way to overlay animation

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,31 +1,46 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useRef } from "react";
-import { motion } from "framer-motion";
+import { useRefs } from "react";
+import { motion, useAnimation } from "framer-motion";
 import styles from "./ListItem.module.scss";
 
 const ListItem = ({ name, index } = props) => {
-	const itemRef = useRef(name);
 	const router = useRouter();
+	const controls = useAnimation();
 
 	const handleClick = (e) => {
-		// console.log("clicked ref" + itemRef.current);
 		console.log("clicked index" + index);
 		e.preventDefault();
+
+		controls.start({
+			"z-index": 10,
+			// position: "absolute",
+			// top: "0",
+			// left: "50%",
+			y: "-100%",
+			scale: 6,
+			transition: { duration: 1 },
+		});
+
+		//z-index 10 this image
+		//tell parent to show overlay
+		//animate to top of screen
 
 		// router.push(`/${name}`);
 	};
 
 	return (
 		<div className={styles.list_item} onClick={handleClick}>
-			<Image
-				src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
-					index + 1
-				}.svg`}
-				width="200"
-				height="200"
-				className="image"
-			/>
+			<p>{name}</p>
+			<motion.div animate={controls} className={styles.list_item__image}>
+				<Image
+					src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+						index + 1
+					}.svg`}
+					width="150"
+					height="150"
+				/>
+			</motion.div>
 		</div>
 	);
 };
